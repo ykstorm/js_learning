5b069e9ef44ba378baea8d01ce329b5924cdd658
ykstorm <150591953+ykstorm@users.noreply.github.com>
2024-07-02 12:05:38 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) we are using nodejs and not browser hence will show error
+console.log(3+3);  // code must be readable
+let name = "laksh"
+let age = 20
+let isloggedin = false
+console.table([name,age,isloggedin])
+console.log(typeof age)
