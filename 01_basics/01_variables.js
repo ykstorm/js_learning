c28f7a07b4888c54c287705ef9bb85a04e15b8c3
ykstorm <150591953+ykstorm@users.noreply.github.com>
2024-07-02 11:23:08 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 12345
+let accountemail = "hs@gmai.com"
+var accountpass = "12345"
+accountcity = "jai"
+console.table([accountId,accountemail,accountpass,accountcity])
+console.log(accountId)
