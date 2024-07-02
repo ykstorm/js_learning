171b8c10ef9be28cc2c8f69ababeef4c7ab99f3e
ykstorm <150591953+ykstorm@users.noreply.github.com>
2024-07-02 12:57:57 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,25 @@
+// let score = "33"
+// console.log(typeof score);
+// console.log(typeof (score))
+// let valueinnumber = Number(score)
+// console.log(typeof valueinnumber)
+// console.log(valueinnumber)
+
+// let score = "ajjs"
+// console.log(typeof score);
+// console.log(typeof (score))
+// let valueinnumber = Boolean(score)
+// console.log(typeof valueinnumber)
+// console.log(valueinnumber)
+
+// for a numer to boolean => 1 is true , 0 is false
+// for a string to boolean => empty string is flase , a string is true
+
+let score = ""
+console.log(typeof score);
+console.log(typeof (score))
+let valueinnumber = String(score)
+console.log(typeof valueinnumber)
+console.log(valueinnumber)
+
+
