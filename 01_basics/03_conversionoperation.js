c3ea36f98ccccef2723f5b98d142e02337119d1a
ykstorm <150591953+ykstorm@users.noreply.github.com>
2024-07-02 13:14:11 +0000

    conversions

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -15,11 +15,27 @@
 // for a numer to boolean => 1 is true , 0 is false
 // for a string to boolean => empty string is flase , a string is true
 
-let score = ""
-console.log(typeof score);
-console.log(typeof (score))
-let valueinnumber = String(score)
-console.log(typeof valueinnumber)
-console.log(valueinnumber)
+// let score = ""
+// console.log(typeof score);
+// console.log(typeof (score))
+// let valueinnumber = String(score)
+// console.log(typeof valueinnumber)
+// console.log(valueinnumber)
 
+// **************************operations********************************
+
+let value = 3
+let negvalue =  -value
+// console.log(negvalue) 
+
+let str1 = "hello"
+let str2 = "user"
+let str3 = str1 +" " +str2
+
+console.log(str3)
+
+console.log(1+2)
+console.log("1"+2)
+console.log(1+2+"2")
+console.log(1+"2"+2)
 
