0f10925da8046d1c22ac74538d62e90cd15b79b5
ykstorm <150591953+ykstorm@users.noreply.github.com>
2024-07-05 12:32:24 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,20 @@
+// let name = "laskh"
+// let myerpocount = 50
+// console.log(`my name is ${name} and repocount is ${myerpocount}`);
+// creating object
+const game = new String('lakshyaraj');
+console.log(game.toUpperCase());
+console.log(game.charAt(3)) //to check at this index what character is present
+console.log(game.indexOf('h')) // to check character h is present at which index
+const newstring = game.substring(0,4) // assinging newstring variable the var game
+console.log(newstring);
+//const anotherstring = game.slice(-2 , -7);
+//console.log(anotherstring);
+const differentstring = "     hitesh   "
+console.log(differentstring)
+console.log(differentstring.trim())
+
+const url = "https://hitesh.com/hitesh%20chooudhary"
+console.log(url.replace('%20','-'))
+console.log(game.includes('lask'))
+
