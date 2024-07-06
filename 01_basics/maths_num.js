785cc63fd121c607cdeb99db592621db5493be0b
ykstorm <150591953+ykstorm@users.noreply.github.com>
2024-07-06 12:22:32 +0000

    numbers in js

---
+++ 01_basics/maths_num.js
@@ -0,0 +1,8 @@
+const score = 400
+const balance = new  Number(100)
+console.log(balance)
+console.log(score)
+console.log(balance.toString().includes(100))
+console.log(balance.toFixed(2))
+const othernumer = 123.4
+console.log(othernumer.toPrecision(4))
