104a7296e86d83f5974919583208b47d4bbe18ca
ykstorm <150591953+ykstorm@users.noreply.github.com>
2024-07-08 12:14:37 +0000

    math

--- 01_basics/maths_num.js
+++ 01_basics/maths_num.js
@@ -1,8 +1,22 @@
-const score = 400
-const balance = new  Number(100)
-console.log(balance)
-console.log(score)
-console.log(balance.toString().includes(100))
-console.log(balance.toFixed(2))
-const othernumer = 123.4
-console.log(othernumer.toPrecision(4))
+// const score = 400
+// const balance = new  Number(100)
+// console.log(balance)
+// console.log(score)
+// console.log(balance.toString().includes(100))
+// console.log(balance.toFixed(2))
+// const othernumer = 123.4
+// console.log(othernumer.toPrecision(3))
+// console.log(Math.abs(-4)) //converts negative to positive
+// console.log(Math.abs(2)) //  wheras postivie remains same
+// console.log(Math.round(4.6)) // here it  rounds off to 5
+// console.log(Math.ceil(4.6)) // here it returns the ceiling value which is 5
+// // console.log(Math.floor(4.6)) // here it returns the floor value of 4
+// // console.log(Math.min(3,4,67,3))
+
+console.log(Math.floor(Math.random()*10) +1) // to remove the possiblity of number being 0
+const min = 10
+const max = 20
+
+console.log(Math.floor( ( Math.random()*((max - min) + 1)) + min)) // max-mmin+1 to give the range and remove zero with the plus 1
+// wheras the +min so it styas in the range of the numbers given and not like the above one
+
