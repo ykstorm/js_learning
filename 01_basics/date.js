cc70955c4ca613c98319d0a764eef23d842d0d7b
ykstorm <150591953+ykstorm@users.noreply.github.com>
2024-07-08 13:28:08 +0000

    date and time

---
+++ 01_basics/date.js
@@ -0,0 +1,23 @@
+let mydate = new Date()
+// console.log(mydate.toString()) //Mon Jul 08 2024 12:44:50 GMT+0000 (Coordinated Universal Time)
+// console.log(mydate.toISOString()) //2024-07-08T12:44:50.743Z
+// console.log(mydate.toJSON()) //2024-07-08T12:44:50.743Z
+// console.log(mydate.toLocaleDateString()) //7/8/2024
+// console.log(mydate.toLocaleString()) //7/8/2024, 12:44:50 PM
+// console.log(mydate.toLocaleTimeString()) //12:44:50 PM
+// console.log(mydate.toTimeString()) //12:44:50 GMT+0000 (Coordinated Universal Time)
+// console.log(mydate.toUTCString()) //Mon, 08 Jul 2024 12:44:50 GMT
+// console.log(mydate.getTimezoneOffset()) //0
+// console.log(mydate.toDateString()) //Mon Jul 08 2024
+
+// console.log(typeof(mydate));
+let newdate = new Date()
+// console.log(newdate.toDateString())
+// console.log(newdate.toLocaleDateString())
+let mytimestamp = Date.now()
+// console.log(mytimestamp)
+// console.log(newdate.getTime())
+// console.log(newdate.getFullYear())
+// console.log(newdate.getDate())
+// console.log(newdate.getHours())
+
