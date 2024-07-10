69f8e0ebca2e7861b4308e6608d3d781824aa30e
ykstorm <150591953+ykstorm@users.noreply.github.com>
2024-07-10 12:15:54 +0000

    array pt 1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,15 @@
+const myArray = [0,1,2,3,true,"hiteshs"]
+// console.log( myArray);
+const myarr = new Array(1,2,3,4,5,6)
+// console.log(typeof myarr)
+// types to declare array
+// myarr.push(34) inserts element to the array 
+// myarr.pop() deletes the last elemnt in the array
+// myarr.unshift(3)
+// console.log(myarr.includes(3))
+// console.log(myarr)
+const myn1 = myarr.splice(1,3)
+console.log("a",myn1)
+console.log(myarr)
+
+
