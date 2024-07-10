5ab8b5512ed4ee8d796a7d4db59951273bfa05c6
ykstorm <150591953+ykstorm@users.noreply.github.com>
2024-07-10 13:11:33 +0000

    array pt2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,14 @@
+const marvel_heros = ["spidey","thor","ironman"]
+const dc_heros = ["superman","flash","batman"]
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+// console.log(marvel_heros[3][1]);
+// console.log(marvel_heros[2]);
+//  const newheros =   marvel_heros.concat(dc_heros)
+// console.log(newheros);
+// const all_heros = [...marvel_heros,...dc_heros] //spread operator
+// console.log(all_heros)
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+console.log(another_array[3][2])
+console.log(another_array[5][2][1])
+
