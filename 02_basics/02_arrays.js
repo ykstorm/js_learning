6926c054e6cf60628b3900046ce6390a7f37dc06
ykstorm <150591953+ykstorm@users.noreply.github.com>
2024-07-12 12:05:31 +0000

    array

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -10,5 +10,19 @@ const dc_heros = ["superman","flash","batman"]
 // console.log(all_heros)
 const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
 console.log(another_array[3][2])
-console.log(another_array[5][2][1])
+console.log(another_array[5][2][1]) //to find elements in the array
+const another_real_array = another_array.flat(Infinity)
+console.log(another_real_array)
+
+console.log(Array.isArray("hitesh"))
+console.log(Array.from("hitesh"))
+console.log(Array.of({name : "hitesh"}))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3))
+
+
+
 
